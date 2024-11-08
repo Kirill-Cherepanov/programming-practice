40f0757232065dc6ba5c5e74bfd5f3074ee2e1ed
Kirill Cherepanov <kirill.cherepanov@imbuesystems.com>
2024-11-08 22:45:11 +0300

    feat: add prepend and append to a linked list

--- theory/dataStructures/linkedList/index.ts
+++ theory/dataStructures/linkedList/index.ts
@@ -12,4 +12,14 @@ export default class LinkedList<T> {
     while (next?.next) next = next.next;
     return next ?? null;
   }
+
+  public prepend(head: ListNode<T>): void {
+    if (this.head) head.next = this.head;
+    this.head = head;
+  }
+
+  public append(next: ListNode<T>): void {
+    if (!this.head) this.head = next;
+    else this.tail!.next = next;
+  }
 }
