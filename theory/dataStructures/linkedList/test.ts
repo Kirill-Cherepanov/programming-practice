e2fd284a5de07e5e16ac93f4d1f44eecfb684168
Kirill Cherepanov <kirill.cherepanov@imbuesystems.com>
2024-11-09 19:33:31 +0300

    feat: add basic tests for the linked list

--- theory/dataStructures/linkedList/test.ts
+++ theory/dataStructures/linkedList/test.ts
@@ -1,3 +1,4 @@
+import LinkedList from './index';
 import ListNode from './node';
 
 describe('ListNode', () => {
@@ -12,5 +13,28 @@ describe('ListNode', () => {
     const node2 = new ListNode(10);
     node1.next = node2;
     expect(node1.next).toBe(node2);
+  });
+});
+
+describe('LinkedList', () => {
+  let list: LinkedList<number>;
+
+  beforeEach(() => {
+    list = new LinkedList<number>();
+  });
+
+  describe('constructor', () => {
+    it('should create an empty linked list by default', () => {
+      expect(list.length).toBe(0);
+      expect(list.head).toBeNull();
+      expect(list.tail).toBeNull();
+    });
+
+    it('should create a linked list with a head value if provided', () => {
+      const listWithHead = new LinkedList(5);
+      expect(listWithHead.head).toBe(5);
+      expect(listWithHead.tail).toBe(5);
+      expect(listWithHead.length).toBe(1);
+    });
   });
 });
